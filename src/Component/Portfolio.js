1c4479031a6d7f755e614aae6075f0789d321d52
Marco-SKN <marcosu234@gmail.com>
2021-09-27 15:09:40 +0800

    added projects & profile pic

--- src/Component/Portfolio.js
+++ src/Component/Portfolio.js
@@ -1,7 +1,12 @@
 import React from "react";
 import creative from "../Image/Creative Agency.jpg";
 import s11 from "../Image/S11_pe.jpg";
+import breakingBad from "../Image/Breaking bad.jpg";
 import taskTracker from "../Image/Task tracker.jpg";
+import ballonDor from "../Image/Ballon dOr.jpg";
+import dadJoke from "../Image/Dad joke.jpg";
+import randomizer from "../Image/Randomizer.JPG";
+import xbox_ps from "../Image/xbox ps.jpg";
 import Project from "./Project";
 
 const Portfolio = () => {
@@ -12,28 +17,48 @@ const Portfolio = () => {
         <Project
           image={creative}
           link="https://marco-skn.github.io/Creative_Agency/"
+          language="HTML, CSS, Javascript"
         />
         <Project
           image={s11}
           link="https://marco-skn.github.io/S11-pe_reactJS/"
+          language="ReactJs"
+        />
+        <Project
+          image={breakingBad}
+          link="https://marco-skn.github.io/Breaking-Bad/"
+          language="ReactJs"
+        />
+        <Project
+          image={ballonDor}
+          link="https://marco-skn.github.io/Ballon-dOr/"
+          language="HTML, CSS, Javascript"
         />
         <Project
           image={taskTracker}
           link="https://marco-skn.github.io/self-task-tracker/"
+          language="ReactJs"
         />
         <Project
-          image={creative}
-          link="https://marco-skn.github.io/Creative_Agency/"
+          image={dadJoke}
+          link="https://marco-skn.github.io/Dad-jokes/"
+          language="HTML, CSS, Javascript"
         />
         <Project
-          image={s11}
-          link="https://marco-skn.github.io/S11-pe_reactJS/"
+          image={randomizer}
+          link="https://marco-skn.github.io/Randomizer/"
+          language="HTML, CSS, Javascript"
         />
         <Project
-          image={taskTracker}
-          link="https://marco-skn.github.io/self-task-tracker/"
+          image={xbox_ps}
+          link="https://marco-skn.github.io/xbox-ps/"
+          language="HTML, CSS, Javascript"
         />
       </div>
+      <h3>
+        View more projects on my{" "}
+        <a href="https://github.com/Marco-SKN">github</a>
+      </h3>
     </section>
   );
 };
--- src/Component/Project.js
+++ src/Component/Project.js
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import Aos from "aos";
 import "aos/dist/aos.css";
 
-const Project = ({ image, link }) => {
+const Project = ({ image, link, language }) => {
   useEffect(() => {
     Aos.init({ duration: 2000 });
   }, []);
@@ -12,7 +12,7 @@ const Project = ({ image, link }) => {
       <img src={image} alt="" />
       <div className="launch">
         <a href={link}>Launch</a>
-        <span>HTML, CSS</span>
+        <span>{language}</span>
       </div>
     </div>
   );
